0e4c882b484c731b36d9b8bbb95b62d65643a388
Maciej Kraskiewicz <maciek.kraskiewicz@gmail.com>
2018-07-18 17:28:43 +0200

    Docker Compose

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,10 +1,15 @@
 package guru.springframework.controllers;
 
+import guru.springframework.model.events.PageViewEvent;
+import guru.springframework.pageview.PageViewService;
 import guru.springframework.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+import java.util.UUID;
 
 /**
  * Created by Maciej on 12/07/2018
@@ -13,14 +18,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     private ProductService productService;
-
+    private PageViewService pageViewService;
     @Autowired
-    public IndexController(ProductService productService) {
+    public IndexController(ProductService productService, PageViewService pageViewService) {
         this.productService = productService;
+        this.pageViewService = pageViewService;
     }
 
     @RequestMapping({"/", "index"})
     public String getIndex(Model model){
+
+        PageViewEvent pageViewEvent = new PageViewEvent();
+        pageViewEvent.setPageViewDate(new Date());
+        pageViewEvent.setPageUrl("springframework.guru");
+        pageViewEvent.setCorrelationId(UUID.randomUUID().toString());
+
+        pageViewService.sendPageViewEvent(pageViewEvent);
+
 
         model.addAttribute("products", productService.listProducts());
 
